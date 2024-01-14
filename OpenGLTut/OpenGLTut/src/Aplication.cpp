66540f69a5f58f4a1d63a3ebb33a8411d6ba2e4d
Weh1D <haticv92@gmail.com>
2024-01-14 17:56:16 +0100

    Index Buffers

--- OpenGLTut/OpenGLTut/src/Aplication.cpp
+++ OpenGLTut/OpenGLTut/src/Aplication.cpp
@@ -125,25 +125,55 @@ int main(void)
 
     std::cout << "GL VESRION: " << glGetString(GL_VERSION) << std::endl;
     
-    float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+    float positions[] = {
+        -0.5f, -0.5f, // 0
+         0.5f, -0.5f, // 1
+         0.5f,  0.5f, // 2
+         -0.5f, 0.5f, // 3
     };
 
+    // By specifing which vertices will be used in the creation of the triangles
+    // we can avoid having to duplicate the vertices in the 'positions'/vertex array
+    unsigned int indexBuffer[] = {
+        0, 1, 2, // first triangle
+        2, 3, 0 // second triangle
+    };
+
+    // ++++++++++++++++++++ SUMMARY ON BUFFER BINDING ++++++++++++++++++++++++++++++++++++++++++++
+    //
+    // 1. The vertex buffer (buffer) needs to be bound before calling glVertexAttribPointer 
+    //    and enabling the vertex attribute array (glEnableVertexAttribArray).
+    // 2. The index buffer (iBuffer) needs to be bound before calling glDrawElements.
+    //
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+    //=================================== CREATING A BUFFER =============================================================
     unsigned int buffer;
     // Create a vertexx buffer object (how many buffers, pointer to int where to save the id/name of the buffer)
     glGenBuffers(1, &buffer);
     // We need to make the buffer an active object in order to store data to it
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     // Now we can copy data to the buffer, it needs a size in bytes, data and usage pattern
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-    
+    glBufferData(GL_ARRAY_BUFFER, 8  * sizeof(float), positions, GL_STATIC_DRAW);
+  
+
     // In order for our gpu to understafnd the structure of our vertex data, we need to describe them
     glEnableVertexAttribArray(0);
     // Reference: https://docs.gl/gl4/glVertexAttribPointer
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    //==================================== CREATING A INDEX BUFFER ============================================================
+   // Binding the index buffer so it can be used 
+    unsigned int iBuffer;
+    glGenBuffers(1, &iBuffer);
+    // A change here when compared to the vertex array buffer is the use of 'GL_ELEMENT_ARRAY_BUFFER'
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indexBuffer, GL_STATIC_DRAW);
+
+    //================================================================================================
+
+    
     // if we use a relative path to a file like this, its relative to the working directory of the project
     // that can be seen under project -> settings -> debugging -> Working Directory
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
@@ -160,7 +190,12 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // render primitives from array data
+        // Reference: https://docs.gl/gl4/glDrawArrays
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // In order to use index array, we need to use glDrawElements insted of glDrawArrays
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
